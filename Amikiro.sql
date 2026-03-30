83e9593b7b4153cb511da6d22cc2c9511d5b007a
Simon <azersimon.56@gmail.com>
2026-03-30 11:46:53 +0200

    ajout SQL

---
+++ Amikiro.sql
@@ -0,0 +1,253 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : db
+-- Généré le : lun. 30 mars 2026 à 09:39
+-- Version du serveur : 8.4.7
+-- Version de PHP : 8.3.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `Amikiro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Bat`
+--
+
+CREATE TABLE `Bat` (
+  `IdBat` int NOT NULL,
+  `Name` varchar(256) NOT NULL,
+  `BirthDate` datetime NOT NULL,
+  `Sex` varchar(10) NOT NULL,
+  `Weight` int NOT NULL,
+  `Note` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Category`
+--
+
+CREATE TABLE `Category` (
+  `IdCatergory` int NOT NULL,
+  `Name` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ColonySection`
+--
+
+CREATE TABLE `ColonySection` (
+  `IdSection` int NOT NULL,
+  `IdCategory` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Logs`
+--
+
+CREATE TABLE `Logs` (
+  `IdLogs` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Section`
+--
+
+CREATE TABLE `Section` (
+  `IdSection` int NOT NULL,
+  `Title` varchar(256) NOT NULL,
+  `Content` varchar(4096) NOT NULL,
+  `CreationDate` datetime NOT NULL,
+  `IdUser` int NOT NULL,
+  `IdLogs` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `SpecimenSection`
+--
+
+CREATE TABLE `SpecimenSection` (
+  `IdSection` int NOT NULL,
+  `IdBat` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `User`
+--
+
+CREATE TABLE `User` (
+  `IdUser` int NOT NULL,
+  `IdRole` int NOT NULL,
+  `Mail` varchar(50) NOT NULL,
+  `Uptime` int NOT NULL,
+  `Pasword` varchar(256) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Surname` varchar(100) NOT NULL,
+  `CountConnect` int NOT NULL,
+  `MemberId` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Video`
+--
+
+CREATE TABLE `Video` (
+  `IdVideo` int NOT NULL,
+  `Title` varchar(100) NOT NULL,
+  `OpenDate` datetime NOT NULL,
+  `CloseDate` datetime NOT NULL,
+  `URL` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Bat`
+--
+ALTER TABLE `Bat`
+  ADD PRIMARY KEY (`IdBat`);
+
+--
+-- Index pour la table `Category`
+--
+ALTER TABLE `Category`
+  ADD PRIMARY KEY (`IdCatergory`);
+
+--
+-- Index pour la table `ColonySection`
+--
+ALTER TABLE `ColonySection`
+  ADD KEY `SectionConstraintColony` (`IdSection`),
+  ADD KEY `CategoryConstraint` (`IdCategory`);
+
+--
+-- Index pour la table `Logs`
+--
+ALTER TABLE `Logs`
+  ADD PRIMARY KEY (`IdLogs`);
+
+--
+-- Index pour la table `Section`
+--
+ALTER TABLE `Section`
+  ADD PRIMARY KEY (`IdSection`),
+  ADD KEY `UserConstraint` (`IdUser`),
+  ADD KEY `LogsConstraint` (`IdLogs`);
+
+--
+-- Index pour la table `SpecimenSection`
+--
+ALTER TABLE `SpecimenSection`
+  ADD KEY `BatConstraint` (`IdBat`),
+  ADD KEY `SectionConstraint` (`IdSection`);
+
+--
+-- Index pour la table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`IdUser`);
+
+--
+-- Index pour la table `Video`
+--
+ALTER TABLE `Video`
+  ADD PRIMARY KEY (`IdVideo`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Bat`
+--
+ALTER TABLE `Bat`
+  MODIFY `IdBat` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Category`
+--
+ALTER TABLE `Category`
+  MODIFY `IdCatergory` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Logs`
+--
+ALTER TABLE `Logs`
+  MODIFY `IdLogs` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Section`
+--
+ALTER TABLE `Section`
+  MODIFY `IdSection` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `User`
+--
+ALTER TABLE `User`
+  MODIFY `IdUser` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `Video`
+--
+ALTER TABLE `Video`
+  MODIFY `IdVideo` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `ColonySection`
+--
+ALTER TABLE `ColonySection`
+  ADD CONSTRAINT `CategoryConstraint` FOREIGN KEY (`IdCategory`) REFERENCES `Category` (`IdCatergory`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `SectionConstraintColony` FOREIGN KEY (`IdSection`) REFERENCES `Section` (`IdSection`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `Section`
+--
+ALTER TABLE `Section`
+  ADD CONSTRAINT `LogsConstraint` FOREIGN KEY (`IdLogs`) REFERENCES `Logs` (`IdLogs`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `UserConstraint` FOREIGN KEY (`IdUser`) REFERENCES `User` (`IdUser`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `SpecimenSection`
+--
+ALTER TABLE `SpecimenSection`
+  ADD CONSTRAINT `BatConstraint` FOREIGN KEY (`IdBat`) REFERENCES `Bat` (`IdBat`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `SectionConstraint` FOREIGN KEY (`IdSection`) REFERENCES `Section` (`IdSection`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
