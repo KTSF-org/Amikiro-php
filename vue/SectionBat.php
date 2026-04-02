83b9eb3fe840746d8243d20be0b30113616028e2
Simon <azersimon.56@gmail.com>
2026-04-02 16:31:21 +0200

    vue sectionBat

--- vue/SectionBat.php
+++ vue/SectionBat.php
@@ -8,6 +8,53 @@
 
 <div class="container">
 
-    Création fiche chauve-souris (formulaire avec les champs à remplir).
+    <p>
+    Création fiche chauve-souris (formulaire avec les champs à remplir). </br>
+    Ce menu s'affiche si l'urilisateur souhaite enregistrer une nouvelle chauve-souris
+    </p>
+
+    <div class="">
+
+        <form method="POST" action="sectionBat">
+
+            <div class="mb-3">
+                <label for="name" class="form-label">Nom de la chauve-souris</label>
+                <input type="text" class="form-control" id="batName" placeholder="Nom">
+            </div>
+
+            <div class="mb-3">
+                <label for="birthDate" class="form-label">Date de naissance de la chauve-souris</label>
+                <input type="text" class="form-control" id="batBirthDate" placeholder="Date de naissance">
+            </div>
+
+            <div class="mb-3">
+                <label for="sex" class="form-label">Sexe de la chauve-souris</label>
+                </br>
+                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="btnradio1">Femelle</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="btnradio2">Mâle</label>
+
+                    <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="btnradio3">Inconnu</label>
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="weight" class="form-label">Masse de la chauve-souris</label>
+                <input type="text" class="form-control" id="batWeight" placeholder="Masse">
+            </div>
+
+            <div class="mb-3">
+                <label for="notes" class="form-label">Autres notes à propos de la chauve-souris</label>
+                <textarea class="form-control" id="batNotes" rows="3" placeholder="Notes"></textarea>
+            </div>
+
+            <button type="button" class="btn btn-primary">Enregistrer la chauve-souris</button>
+
+        </form>
+    </div>
 
 </div>
--- vue/common/header.php
+++ vue/common/header.php
@@ -11,7 +11,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="<?= ASSET ?>/img/favicon.ico" type="image/x-icon" />
     <title><?= $title ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link href="asset/lib/bootstrap/css/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= ASSET ?>/css/popup.css">
     <link rel="stylesheet" href="<?= ASSET ?>/css/main.css">
     <?= $customCSS ?>
