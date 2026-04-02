327313d2996f43165f7523274374179433b680be
Simon <azersimon.56@gmail.com>
2026-04-02 14:26:09 +0200

    correctif sections bat/colony

--- controleur/SectionColony.php
+++ controleur/SectionColony.php
@@ -5,7 +5,7 @@ namespace controleur;
 use app\util\Request as req;
 use vue\base\MainTemplate as Vue;
 
-class Journal {
+class SectionColony {
 
     public function __construct() {
         Vue::render(
