9e81a4aa307c7550d75df624e3a38021e1694a08
Simon <azersimon.56@gmail.com>
2026-04-07 15:40:53 +0200

    avancées journal

--- vue/SectionBat.php
+++ vue/SectionBat.php
@@ -9,52 +9,49 @@
 <div class="container">
 
     <p>
-    Création fiche chauve-souris (formulaire avec les champs à remplir). </br>
-    Ce menu s'affiche si l'utilisateur souhaite enregistrer une nouvelle chauve-souris
+        Création fiche chauve-souris (formulaire avec les champs à remplir). </br>
+        Ce menu s'affiche si l'utilisateur souhaite enregistrer une nouvelle chauve-souris
     </p>
 
-    <div class="">
+    <form method="post" action="SectionBat">
+        <div class="row align-items-start">
 
-        <form method="POST" action="sectionBat">
+            <div class="col">
 
-            <div class="mb-3">
-                <label for="name" class="form-label">Nom de la chauve-souris</label>
-                <input type="text" class="form-control" id="batName" placeholder="Nom">
-            </div>
+                <div class="mb-3">
+                    <label for="title" class="form-label">Titre de la rubrique</label>
+                    <input type="text" class="form-control" id="sectionTitle" placeholder="Titre" name="sectionTitle">
+                </div>
 
-            <div class="mb-3">
-                <label for="birthDate" class="form-label">Date de naissance de la chauve-souris</label>
-                <input type="text" class="form-control" id="batBirthDate" placeholder="Date de naissance">
-            </div>
+                <div class="mb-3">
+                    <label for="observation" class="form-label">Observations</label>
+                    <textarea class="form-control" id="sectionObservation" rows="3" placeholder=""
+                        name="batNotes"></textarea>
+                </div>
 
-            <div class="mb-3">
-                <label for="sex" class="form-label">Sexe de la chauve-souris</label>
-                </br>
-                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="btnradio1">Femelle</label>
-
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="btnradio2">Mâle</label>
-
-                    <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="btnradio3">Inconnu</label>
+                <div class="mb-3">
+                    <div class="form-floating">
+                        <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="sectionCategory">
+                            <option selected>Catégorie</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                        <label for="floatingSelect">Works with selects</label>
+                    </div>
                 </div>
             </div>
 
-            <div class="mb-3">
-                <label for="weight" class="form-label">Masse de la chauve-souris</label>
-                <input type="text" class="form-control" id="batWeight" placeholder="Masse">
+            <div class="col">
+                col 2
+                <a href="<?= $url ?>" role="button" class="btn btn-primary">
+                    Ajouter une nouvelle chauve-souris
+                </a>
             </div>
 
-            <div class="mb-3">
-                <label for="notes" class="form-label">Autres notes à propos de la chauve-souris</label>
-                <textarea class="form-control" id="batNotes" rows="3" placeholder="Notes"></textarea>
-            </div>
+        </div>
+    </form>
 
-            <button type="button" class="btn btn-primary">Enregistrer la chauve-souris</button>
 
-        </form>
-    </div>
 
 </div>
