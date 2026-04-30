30e44fd7cdbae2a1a651eb834d586cf01531eb0a
kevinsio56 <pinelkevin0@gmail.com>
2026-04-30 16:28:18 +0200

    Ajout bouton dans journal (marche pas)

--- vue/journal/Journal.php
+++ vue/journal/Journal.php
@@ -12,7 +12,7 @@
 
     <div class="col m-2">
         <div class="container">
-            <div class="row">
+            <div class="row fw-bold">
                 <div class="col-1 text-center border">
                     #
                 </div>
@@ -32,28 +32,38 @@
                     -
                 </div>
             </div>
-            <?php foreach($listFiches as $fiche){ ?>
-            <div class="row">
-                <div class="col-1 text-center border">
-                    <?=$fiche->getId()?>
+            <?php foreach ($listFiches as $fiche) { ?>
+                <div class="row">
+                    <div class="col-1 text-center border">
+                        <?= $fiche->getId() ?>
+                    </div>
+                    <div class="col border">
+                        <?= $typeAsso[$fiche->getId()] ?>
+                    </div>
+                    <div class="col border">
+                        <?= $fiche->getTitle() ?>
+                    </div>
+                    <div class="col border">
+                        <?= $fiche->getCreationDate() ?>
+                    </div>
+                    <div class="col-2 border">
+                        <?= $usersAsso[$fiche->getIdUser()] ?>
+                    </div>
+                    <div class="col-1 border d-flex justify-content-around align-items-center p-0">
+
+                        <button type="button" class="btn btn-sm" data-bs-toggle="modal" data-bs-target="#modal<?= $id ?>">
+                            <i class="bi bi-eye-fill" width="20px" height="20px" style="color:DodgerBlue;" title="Consulter"></i>
+                        </button>
+                        <a href="<?= $urlModif ?>&id=<?= $id ?>" class="btn btn-sm" style="color:black">
+                            <i class="bi bi-pencil-square" width="20px" height="20px" title="Modifier"></i>
+                        </a>
+                        <a href="<?= $urlDelete ?>&id=<?= $id ?>" class="btn btn-sm" style="color:red">
+                            <i class="bi bi-trash3" width="20px" height="20px" title="Supprimer"></i>
+                        </a>
+
+                    </div>
                 </div>
-                <div class="col border">
-                    <?= $typeAsso[$fiche->getId()] ?>
-                </div>
-                <div class="col border">
-                    <?=$fiche->getTitle()?>
-                </div>
-                <div class="col border">
-                    <?=$fiche->getCreationDate()?>
-                </div>
-                <div class="col-2 border">
-                    <?= $usersAsso[$fiche->getIdUser()]?>
-                </div>
-                <div class="col-1 text-center border">
-                    -
-                </div>
-            </div>
-            <?php }?>
+            <?php } ?>
         </div>
     </div>
 </div>
