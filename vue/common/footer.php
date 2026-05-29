53afffaadf79a201431375efc594fa10a3e80b79
Malteiser <opuutanihiarii@gmail.com>
2026-05-29 16:26:28 +0200

    fix: liens footer relatifs cassés depuis les sous-routes

--- vue/common/footer.php
+++ vue/common/footer.php
@@ -26,16 +26,16 @@
   -->
   <div class="container text-center">
     &copy;<?= date('Y') . ' ' . APP_NAME ?>
-    | <a href="confidentialite" target="_blank">Politique de confidentialité</a>
-    | <a href="mentionslegales" target="_blank">Mentions légales</a>
-    | <a href="credits" target="_blank">Crédits</a>
+    | <a href="<?= $actual_link ?>confidentialite" target="_blank">Politique de confidentialité</a>
+    | <a href="<?= $actual_link ?>mentionslegales" target="_blank">Mentions légales</a>
+    | <a href="<?= $actual_link ?>credits" target="_blank">Crédits</a>
   </div>
 </footer>
 
 <div id="rgpd-banner" class="rgpd-container">
   <p class="mb-0">Ce site respecte votre vie privée : nous ne collectons aucun cookie.</p>
   <div class="rgpd-buttons">
-    <a href="confidentialite" class="btn-link">En savoir plus</a>
+    <a href="<?= $actual_link ?>confidentialite" class="btn-link">En savoir plus</a>
     <button onclick="closeRGPD()" class="btn btn-primary btn-sm" type="button">Fermer</button>
   </div>
 </div>
